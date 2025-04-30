d32cbf3baee16aaa0a5eb8deae9cb5ffb1b40095
Hugo Anes <anesfh@gmail.com>
2025-04-30 19:48:11 +0100

    add webserver

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import asyncio
 from discord import app_commands
 import typing
 import openai  
+import webserver
 
 
 
@@ -367,4 +368,5 @@ async def nft_autocomplete(
     ]
     return choicesnft
 
+webserver.keep_live()
 bot.run(TOKEN)
